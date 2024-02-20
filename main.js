faa63c9682ab79fc0c6549eb2ac5c8dc861df5f6
adityasahusentra <145532705+adityasahusentra@users.noreply.github.com>
2024-02-20 18:57:29 +0530

    added data and resolvers.

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+import _db from "./_db.js";
 
 //server setup
+const resolvers = {
+  Query: {
+    games() {
+      _db.games;
+    },
+    authors() {
+      return _db.authors;
+    },
+    reviews() {
+      return _db.reviews;
+    },
+  },
+};
 const server = new ApolloServer({
   //typedefs - type definitions and data type and the relationship they have with other data types
   typeDefs,
   //resolvers - resolver functions, that determine how we respond to queries for different data on the graph.
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
